4167c819fb51ada455c7ad0deee9e46ac17e66bd
soumya-sunkad <soumyasunkad06@gmail.com>
2025-10-10 19:56:34 +0530

    save

--- labprogram4.py
+++ labprogram4.py
@@ -2,5 +2,9 @@ a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 add=a+b
 sub=a-b
+mult=a*b
+div=a/b
 print("ADDITION:",add)
 print("SUBTRACTION:",sub)
+print("MULTPLICATION:",mult)
+print("DIVISION:",div)
